9b7641afac6b102a1959f7e8383260f7189a5006
Aleksandr Bagrov <bay@balticum-tv.lt>
2022-05-24 11:51:21 +0300

    modify capitalize.test.js added assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,6 +1,3 @@
-// нажмите кнопку run чтобы запустить тесты
-// попробуйте изменять код функции / тестов, запуская проверки заново
-
 import { strict as assert } from 'assert';
 
 import { capitalize } from '../src/capitalize.js';
