18e4dcf43435383f0b07067e03ec18a14629505a
Aleksandr Bagrov <bay@balticum-tv.lt>
2022-05-24 11:49:33 +0300

    modify capitalize.test.js added assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,14 +1,14 @@
 // нажмите кнопку run чтобы запустить тесты
 // попробуйте изменять код функции / тестов, запуская проверки заново
 
+import { strict as assert } from 'assert';
+
 import { capitalize } from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
-
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize(''), '');
+// Первый параметр actual – то, что пришло
+// Второй параметр expected – то, что ожидает тест
+// Правильный порядок аргументов имеет большое значение при анализе ошибки
+assert.equal(capitalize('hello'), 'Hello');
 
 console.log('Все тесты пройдены!');
